0f1e68343456efc1bb2cfb08b61f7ef83034bde6
cysn <Fgaoxing0206@163.com>
2022-09-11 12:02:30 +0800

    up

--- js/login.js
+++ js/login.js
@@ -1,24 +1,34 @@
-const notyf = new Notyf({position: {x: 'right',y: 'top',}});
-fetch('https://core.oblivionocean.top/user/auth',{credentials: "include"}).then(function (response) {
-    response.json().then(function (json) {
-        if (json.status === true) {
-            document.getElementsByClassName('head')[0].style.display = 'block';
-            if (json.data.avatar) {
-                document.getElementsByClassName('head')[0].src = json.data.avatar;
-            }
-        }
-    })
-})
+const notyf = new Notyf({position: {x: 'right', y: 'top',}});
 
-function login() {
-    fetch('https://core.oblivionocean.top/user/login?id='+document.getElementById('login-email').value+'&password='+document.getElementById('login-password').value,{credentials: "include"}).then(function (response) {
+function auth() {
+    fetch('https://core.oblivionocean.top/user/auth', {credentials: "include"}).then(function (response) {
         response.json().then(function (json) {
             if (json.status === true) {
-                notyf.success(json.msg);
+                document.getElementsByClassName('head')[0].style.display = 'block';
+                if (json.data.avatar) {
+                    document.getElementsByClassName('head')[0].src = json.data.avatar;
+                }
+            }
+        })
+    })
+}
+
+function login() {
+    fetch('https://core.oblivionocean.top/user/login?id=' + document.getElementById('login-email').value + '&password=' + document.getElementById('login-password').value, {credentials: "include"}).then(function (response) {
+        response.json().then(function (json) {
+            if (json.status === true) {
+                alert(json.msg);
                 location.reload();
             } else {
-                notyf.error(json.msg);
+                alert(json.msg);
             }
         });
     });
 }
+
+
+function logout() {
+    document.cookie = "key=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=.oblivionocean.top";
+    location.reload();
+}
+
